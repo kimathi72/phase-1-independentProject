e7b65a88f23c8fa80a8aa95668f9dbdeca151ddf
roy kimathi <kimathiwaweru@gmail.com>
2023-04-11 13:20:09 +0300

    updated css classes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".wrapper").style = "margin-top:20%"
+    document.querySelector(".card").style = "margin-top:5%"
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(function(position){
             const lat = position.coords.latitude;
@@ -9,6 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
             return fetchCurrentWeather("http://api.weatherapi.com/v1/current.json?key=2d66208f6e624794bbc92139221612&q=" + lat +"," + long + "&aqi=no"); 
         })
     }
+    document.querySelector("form").addEventListener("submit", (e)=>{
+        e.preventDefault();
+        const cityName = document.querySelector("#citySearch").value;
+        console.log(cityName);
+        return fetchCurrentWeather("http://api.weatherapi.com/v1/current.json?key=2d66208f6e624794bbc92139221612&q=" + cityName + "&aqi=no");
+    })
 })
 
 const fetchCurrentWeather = async (url) => {
@@ -18,31 +24,32 @@ const fetchCurrentWeather = async (url) => {
 }
 
 function renderCurrentWeather(data) {
-    console.log (data);
+    console.log(data);
     updateCurrentLocation(data.location);
     updateWeatherStatus(data.current.condition);
     updateTemperatureDetails(data.current);
     updateWeatherDetails(data.current);
 }
 function updateCurrentLocation(data){
-    document.querySelector("#city").innerHTML= `<span style=\"color: cyan;\" >${data.name}</span> , <span style=\"color: orange;\" >${data.country}</span>`;
-    document.querySelector("#date").innerHTML= `Date: <span style=\"color: cyan;\" >${data.localtime}</span>`
-    document.querySelector(".geo").innerHTML = "<h6>Latitude: <span style=\"color: cyan;\" >" + data.lat + "</span> </h6><h6> " + "Longitude: <span style=\"color: cyan;\" >" + data.lon + "</span></h6>";
+    document.querySelector("#city").innerHTML= `<span style=\"color: orange;\" >${data.name}</span> , <span style=\"color: orange;\" >${data.country}</span>`;
+    document.querySelector("#date").innerHTML= data.localtime
+    document.querySelector(".geo").innerHTML = "<h6><span style=\"color: darkcyan;\" >Latitude: </span>" + data.lat + " </h6><h6> <span style=\"color: darkcyan;\" >" + "Longitude: </span>" + data.lon + "</h6>";
     
 }
 function updateWeatherStatus(data){
-    document.querySelector("#weather-status").innerHTML = `<span style=\"color:orange\">${data.text}</span>`;
+    document.querySelector("#weather-status").innerHTML = `<span style=\"color:darkcyan\">${data.text}</span>`;
     document.querySelector(".weather-icon").src = data.icon;
 
 }
 function updateTemperatureDetails(data){
-    document.querySelector("#temperature").innerHTML = data.feelslike_c;
+    document.querySelector("#temperature").innerHTML = "<span style=\"color: olivedrab\"> "+data.temp_c+"&degC</span>";
 }
 function updateWeatherDetails(data){
-    document.querySelector(".windspeed").innerHTML = data.wind_kph + " km/h"
-    document.querySelector(".humidity").innerHTML = data.humidity + " %";
-    document.querySelector(".pressure").innerHTML = data.pressure_in + " hPa"
-    document.querySelector(".windDirection").innerHTML = data.wind_dir;
-    document.querySelector(".uvIndex").innerHTML = data.uv; 
-    document.querySelector(".windGust").innerHTML = data.gust_kph + " km/h";
+    document.querySelector("#windSpeed").innerHTML = "<span style=\"color: olivedrab\"> "+data.wind_kph + " km/h</span>"
+    document.querySelector("#humidity").innerHTML = "<span style=\"color: olivedrab\"> "+data.humidity + " %</span>";
+    document.querySelector("#pressure").innerHTML = "<span style=\"color: olivedrab\"> "+data.pressure_in + " Inches</span>"
+    document.querySelector("#windDirection").innerHTML = "<span style=\"color: olivedrab\"> "+data.wind_dir+"</span>";
+    document.querySelector("#uvIndex").innerHTML = "<span style=\"color: olivedrab\"> "+data.uv; 
+    document.querySelector("#windGust").innerHTML = "<span style=\"color: olivedrab\"> "+data.gust_kph + " km/h</span>";
+    document.querySelector("#precip").innerHTML = "<span style=\"color: olivedrab\"> "+data.precip_mm + " mm</span>";
 }
